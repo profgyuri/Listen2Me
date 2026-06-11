31a25f1f157a0ec847bc8832f2acdf815fc143e5
György Turbék <profgyuri@gmail.com>
2026-06-12 00:53:18 +0200

    Add settings tabs and view models with DI support in TabsModule

---
+++ Listen2Me.MVVM/ViewModels/Tabs/Settings/AppearanceTabViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Listen2Me.MVVM.ErrorHandling;
+using Serilog;
+
+namespace Listen2Me.MVVM.ViewModels.Tabs.Settings;
+
+public class AppearanceTabViewModel : ViewModelBase
+{
+    public AppearanceTabViewModel(IErrorHandler errorHandler, ILogger logger, IMessenger messenger) 
+        : base(errorHandler, logger, messenger)
+    {
+    }
+}
---
+++ Listen2Me.WPF/Views/Tabs/Settings/AppearanceTab.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Listen2Me.WPF.Views.Tabs.Settings;
+
+public partial class AppearanceTab : UserControl
+{
+    public AppearanceTab()
+    {
+        InitializeComponent();
+    }
+}
