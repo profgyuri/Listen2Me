a992b8058f4416d75b761530a8e3d13a191b0953
György Turbék <profgyuri@gmail.com>
2026-08-06 01:01:55 +0200

    Implemented functionality for existing user controls in Tag Editor

--- Listen2Me.WPF/Views/Layouts/TagEditorLayout.xaml.cs
+++ Listen2Me.WPF/Views/Layouts/TagEditorLayout.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows.Controls;
+using Listen2Me.MVVM.Extensions;
+using Listen2Me.MVVM.Persistence.Entities;
+using Listen2Me.MVVM.ViewModels.Layouts;
 
 namespace Listen2Me.WPF.Views.Layouts;
 
@@ -7,5 +10,12 @@ public partial class TagEditorLayout : UserControl
     public TagEditorLayout()
     {
         InitializeComponent();
+    }
+
+    private void SongGrid_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var vm = (TagEditorLayoutViewModel)DataContext;
+        vm.SelectedSongs.AddRange(e.AddedItems.Cast<Song>());
+        vm.SelectedSongs.RemoveRange(e.RemovedItems.Cast<Song>());
     }
 }
