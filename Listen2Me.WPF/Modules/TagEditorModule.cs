4e3765d894fdbf4a93239d3f61ffb9befc714998
György Turbék <profgyuri@gmail.com>
2026-08-05 18:44:05 +0200

    Add Tag Editor module, view, and navigation integration

---
+++ Listen2Me.WPF/Modules/TagEditorModule.cs
@@ -0,0 +1,25 @@
+﻿using Listen2Me.MVVM.Modules;
+using Listen2Me.MVVM.Navigation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Listen2Me.WPF.Modules;
+
+public class TagEditorModule : IModule
+{
+    public string Name { get; } = "TagEditor";
+    
+    public void RegisterServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void RegisterNavigation(INavigationRegistry registry)
+    {
+        
+    }
+
+    public void RegisterViews(IViewRegistry registry)
+    {
+        
+    }
+}
